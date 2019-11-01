2570ef162304cab13da359c4e3c4df17ed53698c
urmotr <urmotr@tlu.ee>
2019-11-01 09:56:49 +0200

    yl2

--- src/Test2.jsx
+++ src/Test2.jsx
@@ -1,25 +1,41 @@
 import React from "react";
 import {TiHeartFullOutline} from "react-icons/ti";
 
-const Test2 = () => {
-  return (
-    <div>
-      <div className={"description"}>
-        Ülesanne 2:
-        <p>
-          Rakendus peab arvet pidama mitu korda on südame ikooni klikitud ning vastava numbri <code>likes:</code> järel kuvama.
-        </p>
-      </div>
 
-      <div style={{display:"flex", alignItems: "center"}}>
-        <div>
-          Likes: [replace me]
-        </div>
-        <TiHeartFullOutline />
-        <div>Click me</div>
-      </div>
-    </div>
-  );
-};
+class Test2 extends React.PureComponent {
+    constructor(props){
+        super(props);
+        this.state = {
+            likes: 0
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange() {
+        this.setState({
+            likes: this.state.likes+1
+        });
+    }
+    render(){
+        return (
+            <div>
+                <div className={"description"}>
+                    Ülesanne 2:
+                    <p>
+                        Rakendus peab arvet pidama mitu korda on südame ikooni klikitud ning vastava numbri <code>likes:</code> järel kuvama.
+                    </p>
+                </div>
+
+                <div style={{display:"flex", alignItems: "center"}}>
+                    <div>
+                        Likes: {this.state.likes}
+                    </div>
+                    <TiHeartFullOutline  onClick={this.handleChange}/>
+                    <div>Click me</div>
+                </div>
+            </div>
+        );
+    }
+}
 
 export default Test2;
