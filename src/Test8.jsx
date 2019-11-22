7776e405ced12c0a0b2a829d333b607ea6e8d1c5
Harri Saar <harri.saar@cyber.ee>
2019-11-22 09:30:35 +0200

    Test 8

--- src/Test8.jsx
+++ src/Test8.jsx
@@ -5,6 +5,7 @@ class Test8 extends React.PureComponent {
     return (
       <div>
         <Task />
+        implement
       </div>
     );
   }
@@ -18,7 +19,18 @@ const Task = () => (
       Ülesanne 8:
     </h3>
     <ol>
-      <li>draft</li>
+      <li>Seda ülesannet saab lahendada ainult siis kui ülesanne 7 on tehtud</li>
+      <li>Kopeerige ülesandes 7 tehtud vorm <code>test8.jsx</code> faili.</li>
+      <li>Lisage nupp "enable/disable"</li>
+      <li>Kui kasutaja vajutab "disable" nupu peale, siis peab vorm muutuma readonly.
+        Ehk vormi väljadesse ei ole võimalik sisestada uusi väärtuseid ja
+        vormi ei ole võimalik esitada.
+      </li>
+      <li>
+        Kui kasutaja vajutab "enable" nupu peale, siis muutub vorm selliseks, et
+        väljadesse on võimalik sisestada väärtuseid ning vormi on võimalik esitada.
+      </li>
+
     </ol>
   </div>
 );
