0d73f44f5f6d9c7d767d5cb6aa662cf022d2d32e
urmotr <urmotr@tlu.ee>
2019-11-29 09:44:12 +0200

    yl9

--- server/user.router.js
+++ server/user.router.js
@@ -42,5 +42,44 @@ router.post("/t2", (req, res) => {
   res.send(201);
 });
 
+router.post("/t3", (req, res) => {
+  console.log(req.body);
+  const props = {
+    fullName: req.body.nimi,
+    phoneNumber: req.body.phone,
+    address: req.body.elukoht,
+    personalCode: req.body.isikukood,
+  };
+  const user1 = new User(props);
+  User.find({fullName: req.body.nimi}, function (err, users) {
+    if (err) {
+      console.log("Error", err);
+      res.send(500).send(err);
+      return;
+    }
+    if(users.length > 0){
+      console.log(users.length);
+      //User.update({fullName: req.body.nimi}, {personalCode: req.body.isikukood});
+      User.updateOne({fullName: req.body.nimi}, { personalCode: req.body.isikukood }, function(err, affected, resp) {
+        console.log(affected);
+        console.log(err);
+      });
+      console.log("Success updated!");
+      return;
+    }
+    else{
+      user1.save(err => {
+        if (err) {
+          console.log("Error", err);
+          res.sendStatus(500);
+          return;
+        }
+        console.log("Success created!");
+        res.sendStatus(201);
+      });
+    }
+  });
+});
+
 module.exports = router;
 
--- src/tests/Test9.jsx
+++ src/tests/Test9.jsx
@@ -1,11 +1,72 @@
 import React from "react";
 
 class Test9 extends React.PureComponent {
+    constructor(props){
+        super(props);
+        this.state ={
+            isikukood:"",
+            nimi:"",
+            phone: "",
+            elukoht: ""
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log(this.state);
+        fetch("/api/v1/users/t3", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(this.state)
+        })
+            .then(() => {
+                console.log("Kasutaja lisatud");
+            })
+            .catch(err =>{
+                console.log("Error", err);
+            });
+    }
+
+    handleChange(event) {
+        console.log(this.state);
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+
   render() {
     return (
       <div>
         <Task />
-        implement
+          <div className="ds">
+              <form className="ds-item style-2">
+                  <h3 className="style-2">Kasutaja lisamine</h3>
+                  <div className={"row"}>
+                      <label htmlFor="nimi">Nimi</label>
+                      <input value={this.state.nimi} onChange={this.handleChange} name="nimi" type="text" />
+                  </div>
+                  <div className={"row"}>
+                      <label htmlFor="isikukood">Isikukood</label>
+                      <input value={this.state.isikukood} onChange={this.handleChange}  name="isikukood" type="text" />
+                  </div>
+                  <div className={"row"}>
+                      <label htmlFor="phone">Telefon</label>
+                      <input value={this.state.phone} onChange={this.handleChange}  name="phone" type="text" />
+                  </div>
+                  <div className={"row"}>
+                      <label htmlFor="elukoht">Elukoht</label>
+                      <input value={this.state.elukoht} onChange={this.handleChange}  name="elukoht" type="text" />
+                  </div>
+                  <button onClick={this.handleSubmit} style={{width: "100%", marginBottom: "10px"}} value={"Submit"}>
+                      Lisa
+                  </button>
+              </form>
+          </div>
       </div>
     );
   }
